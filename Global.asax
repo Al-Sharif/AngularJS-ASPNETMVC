930ea9091e16c194e3cc9665861f6e115ad66576
Majdi Al-Sharif <majdi.alsharif@hotmail.com>
2016-01-30 17:02:08 +0200

    initial files

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_02_Angular_Getting_Started.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_02_Angular_Getting_Started.Startup))]
+namespace _02_Angular_Getting_Started
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
