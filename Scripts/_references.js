02564f697daaf0b05d2a6fda002d556427f01fad
Majdi Al-Sharif <majdi.alsharif@hotmail.com>
2016-01-30 18:28:40 +0200

    Angular Route Added - Edits on ngApp.js

--- Scripts/_references.js
+++ Scripts/_references.js
@@ -1,5 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="angular.js" />
 /// <reference path="angular-mocks.js" />
+/// <reference path="angular-route.min.js" />
 /// <reference path="controllers/homecontroller.js" />
 /// <reference path="ngapp.js" />
--- Scripts/ngApp.js
+++ Scripts/ngApp.js
@@ -1,1 +1,18 @@
 ﻿var ngApp = angular.module('ngApp', []);
+ngApp.controller('HomeController', HomeController);
+
+var configFunction = Function($routeProvider){
+    $routeProvider.
+        when('/routeOne',{
+            templateUrl: 'routesDemo/one'
+        })
+        .when('/routeTwo', {
+            templateUrl: 'routesDemo/Two'
+        })
+        .when('/routeThree', {
+            templateUrl: 'routesDemo/Three'
+        });
+    
+}
+configFunction.$inject = ['$routeProvider'];
+ngApp.config(configFunction);
