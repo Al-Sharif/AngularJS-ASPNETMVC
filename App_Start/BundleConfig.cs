fb5bb25e2a41404b579f3645c82bcdbc6318fe09
Majdi Al-Sharif <majdi.alsharif@hotmail.com>
2016-01-30 18:10:22 +0200

    -Angular Intellisense added to VS 2015 // see edits on _references.js - create ngApp.js - create Controllers/HomeController.js

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -7,7 +7,10 @@ namespace _02_Angular_Getting_Started
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-           
+            bundles.Add(new ScriptBundle("~/bundles/ngApp")
+                .IncludeDirectory("~/Scripts/Controllers", "*.js")
+                .Include("~/Scripts/ngApp.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/site.css"));
 
